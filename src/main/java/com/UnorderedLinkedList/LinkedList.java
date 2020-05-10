1f8f233227ce59ef26c79648a71c9422a2b5e4a4
sampada8217 <sampadatirthkar@gmail.com>
2020-05-10 19:27:57 +0530

    Refactored Code of function name of testcases and funtions

--- src/main/java/com/UnorderedLinkedList/LinkedList.java
+++ src/main/java/com/UnorderedLinkedList/LinkedList.java
@@ -1,6 +1,8 @@
 package com.UnorderedLinkedList;
 
-    class Node<E extends Comparable<E> >{
+import java.io.IOException;
+
+class Node<E extends Comparable<E> >{
         E data;
         Node next;
         Node(E data){
@@ -10,14 +12,23 @@ package com.UnorderedLinkedList;
         }
         @Override
         public String toString(){
-            return (String) this.data;
+
+            return "LinkedList [data=" +data;
         }
     }
     public class LinkedList<E extends Comparable<E> > {
         public static Node head=null;
         public static Node tail=null;
+        E data;
+       public LinkedList(E data){
+            this.data=data;
+        }
 
-        public static <E extends Comparable<E> > Node list(){
+        public LinkedList() {
+
+        }
+
+        public  <E extends Comparable<E> > Node list(){
 
             if(head==null){
                 head=null;
@@ -26,7 +37,7 @@ package com.UnorderedLinkedList;
             return null;
         }
 
-        public static<E extends Comparable<E> > E add(E data) {
+        public <E extends Comparable<E> > E add(E data) {
             Node newNode=new Node(data);
 
             if(head==null){
@@ -37,7 +48,7 @@ package com.UnorderedLinkedList;
             tail=newNode;
             return data;
         }
-        public static <E extends Comparable<E> > E remove(E data){
+        public <E extends Comparable<E> > E remove(E data){
             Node current=head;
             Node temp=null;
          if(data==current.data){
@@ -50,13 +61,12 @@ package com.UnorderedLinkedList;
             }
             temp.next=current.next;
 
-
             return data;
         }
 
         public <E extends Comparable<E> >  int size() {
-            int size=0;
             Node current=head;
+            int size=0;
             while(current!=null){
                 size++;
                 current=current.next;
@@ -65,31 +75,33 @@ package com.UnorderedLinkedList;
         }
 
         public <E extends Comparable<E> > boolean search(E data) {
-
-            Node current=head;
-            int count=1;
-            if(data==current.data){
+           try {
+               Node current = head;
+               int count = 1;
+               if (data == current.data) {
+                   return true;
+               }
+               while (current != null && current.data != data) {
+                   current = current.next;
+                   count++;
+               }
+           }catch (NullPointerException e){
+               e.printStackTrace();
+           }
                 return true;
-            }
-            while(current!=null && current.data!=data) {
-                current = current.next;
-                count++;
-            }
-                return true;
-            }
+       }
 
 
         public boolean isEmpty() {
-            return size()==0;
+            return head==null;
         }
 
-        public  <E extends Comparable<E> > void append(E data) {
-            Node newNode = new Node(data);
-            if (head != null) {
-                tail.next = newNode;
-                tail = newNode;
+        public  <E extends Comparable<E> > E append(E data) {
+            add(data);
+
+            return data;
             }
-        }
+
 
         public <E extends Comparable<E> >int findIndex(E data) {
             int index=1;
@@ -103,6 +115,27 @@ package com.UnorderedLinkedList;
             }
             return index;
         }
+
+        public  <E extends Comparable<E> > int insert(E data, int pos) {
+            try {
+                Node newNode = new Node(data);
+                Node previous = head;
+                int count = 1;
+                while (count < pos - 1) {
+                    previous = previous.next;
+                    count++;
+                }
+                Node current = previous.next;
+                newNode.next = current;
+                previous.next = newNode;
+
+            }catch (NullPointerException e)  {
+
+                e.printStackTrace();
+            }
+            return pos;
+        }
+
     }
 
 
--- src/test/java/com/UnorderedLinkedList/LinkedListTest.java
+++ src/test/java/com/UnorderedLinkedList/LinkedListTest.java
@@ -5,15 +5,15 @@ import org.junit.Test;
 
 public class LinkedListTest {
     @Test
-    public void testEmptyLinkedList() {
+    public void whenCreate_newLinkedList_shouldReturn_emptyList() {
         LinkedList linkedList=new LinkedList();
-        Node head= LinkedList.list();
+        Node head= linkedList.list();
         Assert.assertEquals(null,head);
 
     }
 
     @Test
-    public void testAddItem() {
+    public void whenAddNewItem_shouldReturn_nothing() {
         LinkedList linkedList=new LinkedList();
         linkedList.add(1);
         linkedList.add(2);
@@ -22,37 +22,51 @@ public class LinkedListTest {
         Assert.assertEquals(4,linkedList.size());
     }
 
-    @Test
-    public void testRemoveItem() {
-        LinkedList linkedList=new LinkedList();
-        linkedList.remove(4);
-        Assert.assertEquals(3, linkedList.size());
 
+    @Test
+    public void whenItem_Removed_fromLinkedList_shouldReturnData() {
+        LinkedList linkedList=new LinkedList();
+        Comparable data=linkedList.remove(4);
+        Assert.assertEquals(4,data);
     }
 
     @Test
-    public void testSearchItem() {
+    public void whenSearchItem_fromLinkedList_shouldReturnsTrue() {
         LinkedList linkedList=new LinkedList();
         Assert.assertTrue(linkedList.search(1));
 
     }
     @Test
-    public void testIsEmpty(){
+    public void whenLinkedList_IsNotEmpty_returnsFalse(){
         LinkedList linkedList=new LinkedList();
         Assert.assertFalse(linkedList.isEmpty());
     }
 
     @Test
-    public void testAppend() {
+    public void whenLinkedList_addItemAtEnd_shouldReturnData() {
         LinkedList linkedList=new LinkedList();
-        linkedList.append(5);
-        Assert.assertEquals(3,linkedList.size());
+        Assert.assertEquals(6,linkedList.append(6));
     }
 
     @Test
-    public void testIndex() {
+    public void whenCheckIndex_returns_positionOfData() {
         LinkedList linkedList=new LinkedList();
-        Assert.assertEquals(5,linkedList.findIndex(5));
+        Assert.assertEquals(2,linkedList.findIndex(2));
+
+    }
+
+   @Test
+    public void whenAddNewItem_atSpecificPos_returnsPosWhereDataAdded() {
+        LinkedList linkedList=new LinkedList();
+        Comparable pos=linkedList.insert(8,6);
+        Assert.assertEquals(6,pos);
+
+    }
+
+    @Test
+    public void whenItemRemoved_shouldReturn_LastItem() {
+        LinkedList linkedList=new LinkedList();
+
 
     }
 }
